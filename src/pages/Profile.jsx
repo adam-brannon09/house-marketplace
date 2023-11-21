0df397279be68b1271cd3e9d4207594203ec4486
adam-brannon09 <adam.brannon09@gmail.com>
2023-11-21 14:06:16 -0600

    added create listing component and some logic

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,12 @@
 import { getAuth, updateProfile } from "firebase/auth"
 import { updateDoc, doc } from 'firebase/firestore'
 import { db } from '../firebase.config'
+
 import { toast } from 'react-toastify'
 import { useState, useEffect } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
+import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg'
+import homeIcon from '../assets/svg/homeIcon.svg'
 
 
 
@@ -81,6 +84,11 @@ function Profile() {
                         />
                     </form>
                 </div>
+                <Link to='/create-listing' className="createListing">
+                    <img src={homeIcon} alt="home" />
+                    <p>Sell or Rent your home</p>
+                    <img src={arrowRight} alt="arrow right" />
+                </Link>
             </main>
         </div>
     )
